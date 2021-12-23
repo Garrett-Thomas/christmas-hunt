c69856b333b02631393aa1cacb10660e9573b0fc
Garrett Thomas <gt@garretts-air.lan>
2021-12-22 22:54:55 -0800

    Hopefully fixed server issue

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000;
 if (process.env.NODE_ENV == 'production') {
 	app.use(express.static('build/'));
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 	});
 }
 
